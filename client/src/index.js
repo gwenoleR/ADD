d59387c9c04d0e2bd950127db1fc0ef4cc360082
Rémi FOUGA <remi.fouga@imerir.com>
2017-10-30 21:13:50 +0100

    add the loop and the json parse

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+var test='[{"pizza_available": true, "pizza_name": "4 Fromages", "pid": "5efc105f-96f2-493a-a643-05d4d2c2b1da", "pizza_description": "Une belle pizza pleine de fromage", "pizza_price": 12.0, "pizza_picture": "test.jpg"}, {"pizza_available": false, "pizza_name": "toDelete", "pid": "", "pizza_description": "delete", "pizza_price": 1.2, "pizza_picture": "test.jpg"}]'
 
 class Pizza extends React.Component {
     render() {
@@ -22,7 +23,16 @@ class Pizza extends React.Component {
       }
       
     render() {
-      return this.renderPizza("la classique", "test.jpg", "base tomate, jambon, fromage, champignon, chorizo, poivron, oignon", "15");
+        //recuperation des data a faire
+        console.log(test);
+        var pizzas=JSON.parse(test);
+        console.log(pizzas);
+        var htmlpizza=[];
+        for(var i=0; i<pizzas.length;i++){
+            htmlpizza.push(this.renderPizza(pizzas[i]["pizza_name"], pizzas[i]["pizza_picture"], pizzas[i]["pizza_description"], pizzas[i]["pizza_price"]));
+        }
+        // a mettre dans une boucle pour chaque pizza recupéré du server
+      return htmlpizza;
         
     
     }
